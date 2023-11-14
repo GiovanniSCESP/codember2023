fd5b2c7c54e41b2aed9ce6d6b5a239e3f4ec70e4
Giovanni <giovanniandre2003@gmail.com>
2023-11-14 20:40:06 +0000

    a

--- reto_02/py/codember_02.py
+++ reto_02/py/codember_02.py
@@ -1,5 +1,13 @@
-mensaje = '&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&'
-entrada = '##*&'
+mensaje = r'&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&'
+entrada = '&##&*&@& '
 
-for element in entrada:
-    print(element)
+res_txt = ''
+res_num = 0
+
+for element in mensaje:
+    if element == '#': res_num += 1
+    elif element == '@': res_num -= 1
+    elif element == '*': res_num *= res_num
+    elif element == '&': res_txt += str(res_num)
+
+print(res_txt)
