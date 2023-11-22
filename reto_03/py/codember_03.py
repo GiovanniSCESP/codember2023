6175fd2d827de9d3e872a58ec2bce622c3eb6eee
GiovanniSCESP <giovanniandre2003@gmail.com>
2023-11-22 00:30:44 +0000

    one liner

--- reto_03/py/codember_03.py
+++ reto_03/py/codember_03.py
@@ -1,9 +1,9 @@
-with open(r'..\file.txt', 'r') as f:
-    values = f.read()
+print([clave for clave in [linea.replace(':','').split() for linea in open(r'..\file.txt').read().splitlines()] if not [int(wrd) for wrd in clave[0].split('-')][0] <= len([wrd for wrd in clave[2] if wrd == clave[1]]) <= [int(wrd) for wrd in clave[0].split('-')][1]][41])
 
-claves = [linea.replace(':','').split() for linea in values.splitlines()]
-print([clave for clave in claves if not [int(wrd) for wrd in clave[0].split('-')][0] <= len([wrd for wrd in clave[2] if wrd == clave[1]]) <= [int(wrd) for wrd in clave[0].split('-')][1]][41])
+# with open(r'..\file.txt', 'r') as f:
+#     values = f.read()
 
+# claves = [linea.replace(':','').split() for linea in values.splitlines()]
 # i = 0
 # for clave in claves:
 #     var_min, var_max = [int(wrd) for wrd in clave[0].split('-')]
